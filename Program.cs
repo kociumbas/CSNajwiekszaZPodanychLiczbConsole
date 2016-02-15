d7b45f537dd5015555e3953d94fb5611ce094ec9
kociumbas <kociumbas@outlook.com>
2016-02-16 00:36:56 +0100

    CSNajwiekszaZPodanychLiczbConsole

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja10i11Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int max = 0;
+            Console.Write("\n\tPodaj liczbę x=");
+            x = Convert.ToInt32(Console.ReadLine());
+            while (x>0)
+                {
+                if (x>max) max = x;
+                Console.Write("\n\tNajwiększa z podanych liczb jest równa:" + max);
+                Console.Write("\n\tPodaj kolejną liczbę x=");
+                x = Convert.ToInt32(Console.ReadLine()); ;
+                };
+            Console.Write("\n\tNaciśnij ENTER, aby zakończyć program");
+            Console.ReadLine();
+//            return 0;
+
+        }
+    }
+}
